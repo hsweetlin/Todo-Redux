51b3fd079e195a1843aeeb16ead92381098ea714
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-03-14 19:40:56 +0530

    chore: update few changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { ToDo } from "./components/ToDo/ToDo";
+import TodoForm from "./components/TodoForm/TodoForm";
+import TodoList from "./components/ToDoList/TodoList";
 
 
 export function App() {
     return(
         <>
         <ToDo/>
+        <TodoForm />
+        <TodoList/>
         </>
     )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from './reducers';
 
 const root: HTMLElement |null = document.getElementById('root');
 const element = ReactDOM.createRoot(root as HTMLElement );
 
+const store = createStore(rootReducer);
+
 element.render(
+     <Provider store={store}>
      <App/> 
+     </Provider>
 )
